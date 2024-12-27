8e23a9acf6c6ddc67c86a157a27cb8c6220045a9
0xCambie <matteratze@gmail.com>
2024-12-27 15:56:05 -0300

    some updates

--- .config/suckless/dwm_doublemon/config.h
+++ .config/suckless/dwm_doublemon/config.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char col_gray1[]       = "#252525";
 static const char col_gray2[]       = "#555555";
 static const char col_gray3[]       = "#7f7f7f";
